0b602bb102275f1aab819614ca26380bb7bd7ae2
Олександр <oleksandr140407@gmail.com>
2024-10-28 08:24:15 +1000

    “lab8.1.rec”

---
+++ Test.rec/Test.rec.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Testrec
+{
+	TEST_CLASS(Testrec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
